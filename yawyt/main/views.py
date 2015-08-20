ec6397d8a509455f02f469b0cf1c6b8c830740d9
Wessel Stoop <wstoop@applejack.science.ru.nl>
2015-08-20 18:23:00 +0200

    Actual tweets are now returned in the results view

--- yawyt/main/views.py
+++ yawyt/main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from models import ClassifierSection
 import yawyt.settings as settings
+from twitter.tweet import file_to_tweet_dict
 from analysis import start_analysis_thread_for_user
 
 # Create your views here.
@@ -24,4 +25,15 @@ def log(request, user):
 
 def results(request,user):
 
-    return render(request,'result_overview.html',{'classifier_sections':ClassifierSection.objects.all()})
+    all_tweets_for_user = file_to_tweet_dict(settings.TWEET_DATAFOLDER+user+'.txt')
+
+    #Add the annotations for all classifiers
+    most_extreme_tweets = {}
+
+    for classifier_section in ClassifierSection.objects.all():
+
+        most_extreme_tweets[classifier_section.classifier_module_name] = all_tweets_for_user.values()[:5]
+        print(most_extreme_tweets)
+
+    return render(request,'result_overview.html',{'classifier_sections':ClassifierSection.objects.all(),
+                                                  'most_extreme_tweets':most_extreme_tweets})
