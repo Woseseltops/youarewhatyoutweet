5241ca3c9915d641cfb28271886232a3a583aef7
Wessel Stoop <wstoop@applejack.science.ru.nl>
2015-08-28 17:53:26 +0200

    Styled the score meter

--- yawyt/main/templatetags/score_visualisation.py
+++ yawyt/main/templatetags/score_visualisation.py
@@ -6,4 +6,15 @@ register = template.Library()
 @register.filter()
 def show_score_visualisation(score,profile_image_url):
 
-    return mark_safe('<div class="scoremeter"><div class="scoremeter-part left_part"></div><div class="scoremeter-part right_part"></div><div style="margin-left:'+str(score)+'px;" class="photo_container"><div class="triangle"></div><div class="user_photo"><img src="'+str(profile_image_url)+'"></div></div></div>')
+    TOTAL_LINE_LENGTH = 500
+    OFFSET_PHOTO_CONTAINER = 14
+
+    length_left_line = score
+    length_right_line = TOTAL_LINE_LENGTH - score
+    photo_container_left = score + OFFSET_PHOTO_CONTAINER
+
+    return mark_safe('<div class="scoremeter"><div style="width:'+str(length_left_line)+'px;" class="scoremeter-part left_part"></div>' +\
+                     '<div class="number_bubble"><div class="number">'+str(score)+'</div></div>' +\
+                     '<div style="width:'+str(length_right_line)+'px;" class="scoremeter-part right_part"></div>'+\
+                     '<div style="margin-left:'+str(photo_container_left)+'px;" class="photo_container"><div class="triangle"></div><div class="user_photo">' +\
+                     '<img src="'+str(profile_image_url)+'"></div></div></div>')
