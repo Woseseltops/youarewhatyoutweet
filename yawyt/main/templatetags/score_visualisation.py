acf75877cf9c74f3306d24d01669a35d845270e2
Wessel Stoop <wstoop@applejack.science.ru.nl>
2015-09-03 17:54:07 +0200

    Some final style changes for the results

--- yawyt/main/templatetags/score_visualisation.py
+++ yawyt/main/templatetags/score_visualisation.py
@@ -9,12 +9,18 @@ def show_score_visualisation(score,profile_image_url):
     TOTAL_LINE_LENGTH = 500
     OFFSET_PHOTO_CONTAINER = 14
 
-    length_left_line = score
-    length_right_line = TOTAL_LINE_LENGTH - score
-    photo_container_left = score + OFFSET_PHOTO_CONTAINER
+    percentage_to_pixel_factor = TOTAL_LINE_LENGTH/100
+
+    length_left_line = score * percentage_to_pixel_factor
+    length_right_line = TOTAL_LINE_LENGTH - score * percentage_to_pixel_factor
+    photo_container_left = score * percentage_to_pixel_factor + OFFSET_PHOTO_CONTAINER
+
+    score_as_str = str(score)
+    if len(score_as_str) < 2:
+        score_as_str = '0'+score_as_str
 
     return mark_safe('<div class="scoremeter"><div style="width:'+str(length_left_line)+'px;" class="scoremeter-part left_part"></div>' +\
-                     '<div class="number_bubble"><div class="number">'+str(score)+'</div></div>' +\
+                     '<div class="number_bubble"><div class="number">'+score_as_str+'</div></div>' +\
                      '<div style="width:'+str(length_right_line)+'px;" class="scoremeter-part right_part"></div>'+\
                      '<div style="margin-left:'+str(photo_container_left)+'px;" class="photo_container"><div class="triangle"></div><div class="user_photo">' +\
                      '<img src="'+str(profile_image_url)+'"></div></div></div>')
