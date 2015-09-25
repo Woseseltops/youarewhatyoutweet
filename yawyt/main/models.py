0b00fd2391978f661de73d83e5f90a686dd7110d
Wessel Stoop <wstoop@applejack.science.ru.nl>
2015-09-25 15:01:22 +0200

    Part of my previous commit about the nr of tweets int he score calculation

--- yawyt/main/models.py
+++ yawyt/main/models.py
@@ -7,6 +7,7 @@ class ClassifierSection(models.Model):
     classifier_class_name = models.CharField(max_length=200)
     template_name = models.CharField(max_length=200)
     position = models.IntegerField(default=0)
+    number_of_tweets_in_score_calculation = models.IntegerField(default=30)
 
     def __str__(self):
         return self.template_name.capitalize();
