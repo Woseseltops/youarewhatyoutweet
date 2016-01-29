feeaee380b59ab09fb03864ec56f79f4f7dcc678
Wessel Stoop <wstoop@applejack.science.ru.nl>
2016-01-29 12:46:05 +0100

    Added percentage signs

--- yawyt/main/templatetags/score_visualisation.py
+++ yawyt/main/templatetags/score_visualisation.py
@@ -6,12 +6,11 @@ register = template.Library()
 @register.filter()
 def show_score_visualisation(score,profile_image_url):
 
-    TOTAL_LINE_LENGTH = 500
-    OFFSET_PHOTO_CONTAINER = 14
+    TOTAL_LINE_LENGTH = 450
+    OFFSET_PHOTO_CONTAINER = 22
 
     percentage_to_pixel_factor = TOTAL_LINE_LENGTH/100
-
-    length_left_line = score * percentage_to_pixel_factor
+    length_left_line = float(score) * percentage_to_pixel_factor
     length_right_line = TOTAL_LINE_LENGTH - score * percentage_to_pixel_factor
     photo_container_left = score * percentage_to_pixel_factor + OFFSET_PHOTO_CONTAINER
 
@@ -20,7 +19,11 @@ def show_score_visualisation(score,profile_image_url):
         score_as_str = '0'+score_as_str
 
     return mark_safe('<div class="scoremeter"><div style="width:'+str(length_left_line)+'px;" class="scoremeter-part left_part"></div>' +\
-                     '<div class="number_bubble"><div class="number">'+score_as_str+'</div></div>' +\
+                     '<div class="number_bubble"><div class="number">'+score_as_str+'<span class="percentage">%</span></div></div>' +\
                      '<div style="width:'+str(length_right_line)+'px;" class="scoremeter-part right_part"></div>'+\
                      '<div style="margin-left:'+str(photo_container_left)+'px;" class="photo_container"><div class="triangle"></div><div class="user_photo">' +\
                      '<img src="'+str(profile_image_url)+'"></div></div></div>')
+
+@register.filter()
+def show_fraction_as_percentage(fraction):
+    return round(fraction * 100)
