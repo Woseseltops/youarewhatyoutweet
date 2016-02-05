f5261deea01d432fbefb46b0e057069fa8d6ae45
Wessel Stoop <wstoop@applejack.science.ru.nl>
2016-02-05 13:38:20 +0100

    A first try at integrating predictability (too slow, crashes halfway)

--- yawyt/main/classifiers/predictability_classifier.py
+++ yawyt/main/classifiers/predictability_classifier.py
@@ -1,9 +1,13 @@
 from main.classifiers.abstract_classifier import Classifier
 from soothsayer.predictability import get_predictability_for_sentence
+from yawyt.settings import SOOTHSAYER_SERVER_URL
 
 class PredictabilityClassifier(Classifier):
     
     name = 'predictability_classifier'
 
     def classify(self,tweet):
-        self.add_classifications_to_tweet(tweet,{'predictability':0.8})
+#        predictability = get_predictability_for_sentence(tweet.content,SOOTHSAYER_SERVER_URL)
+        predictability = 0.4
+        print(predictability)
+        self.add_classifications_to_tweet(tweet,{'predictability':predictability})
