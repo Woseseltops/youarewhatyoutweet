5e5d1f9d976b3bb00c953ac81f79e531b165bc9f
Wessel Stoop <wstoop@applejack.science.ru.nl>
2016-02-05 11:22:04 +0100

    Added empty predictability classifier

---
+++ yawyt/main/classifiers/predictability_classifier.py
@@ -0,0 +1,8 @@
+from abstract_classifier import Classifier
+
+class PredictabilityClassifier(Classifier):
+    
+    name = 'predictability_classifier'
+
+    def classify(self,tweet):
+        self.add_classifications_to_tweet(tweet,{'predictability':0.8})
