a2491de00517b922a9f6cfcbfe6d8e5b34e06b54
Wessel Stoop <wstoop@applejack.science.ru.nl>
2016-07-15 11:34:41 +0200

    Less succesful playing around with predictability

--- yawyt/main/classifiers/predictability_classifier.py
+++ yawyt/main/classifiers/predictability_classifier.py
@@ -1,4 +1,5 @@
 from main.classifiers.abstract_classifier import Classifier
+from yawyt.settings import SOOTHSAYER_BASE_LOCATION, SOOTHSAYER_MODEL_LOCATION
 from soothsayer.predictability import get_predictability_for_sentence
 
 class PredictabilityClassifier(Classifier):
@@ -6,6 +7,6 @@ class PredictabilityClassifier(Classifier):
     name = 'predictability_classifier'
 
     def classify(self,tweet):
-        predictability = get_predictability_for_sentence(tweet.content)
+        predictability = get_predictability_for_sentence(tweet.content,SOOTHSAYER_BASE_LOCATION,SOOTHSAYER_MODEL_LOCATION)
         print('predictability',predictability)
         self.add_classifications_to_tweet(tweet,{'predictability':predictability})
