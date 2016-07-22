6fb781d6aba50dd40dc6f928cdb5dee707733578
Wessel Stoop <wstoop@applejack.science.ru.nl>
2016-07-22 12:48:43 +0200

    Updated view and db

--- yawyt/main/views.py
+++ yawyt/main/views.py
@@ -99,38 +99,6 @@ def results_overview(request,user):
     if user in [None,'']:
         return HttpResponse('Er is een probleem. Heb je misschien een afgeschermd account?')
 
-    all_tweets_for_user = file_to_tweet_dict(settings.TWEET_DATAFOLDER+user+'.txt')
-
-    most_extreme_tweets = {}
-    meterscores_per_class = {}
-
-    #Add the annotations for all classifiers
-    for classifier_section in ClassifierSection.objects.all():
-
-        classifier_name = classifier_section.classifier_module_name
-        add_annotations_in_files_to_tweets(settings.CLASSIFICATION_DATAFOLDER+user+'.'+classifier_name+'.txt',classifier_name,all_tweets_for_user)
-
-        #Prepare saving the most extreme scores for each class for this classifier
-        most_extreme_tweets[classifier_name] = {}
-        meterscores_per_class[classifier_name] = {}
-
-        #See what classes there are for this classifier, by taking them from a random tweet
-        classes_for_this_classifier = list(all_tweets_for_user[list(all_tweets_for_user.keys())[0]].automatic_classifications[classifier_name].keys())
-
-        #For each class, take the most extreme cases
-        for classname in classes_for_this_classifier:
-            all_tweets_sorted_by_confidence_for_this_class = sorted(all_tweets_for_user.values(),key=lambda tweet: tweet.automatic_classifications[classifier_name][classname], reverse = True)
-            most_extreme_tweets[classifier_name][classname] = all_tweets_sorted_by_confidence_for_this_class[:settings.NUMBER_OF_TWEETS_TO_SHOW_PER_CLASS]
-
-            if classifier_section.number_of_tweets_in_score_calculation == 0:
-                tweets_to_use_for_meter_score = all_tweets_sorted_by_confidence_for_this_class
-            else:
-                tweets_to_use_for_meter_score = all_tweets_sorted_by_confidence_for_this_class[:classifier_section.number_of_tweets_in_score_calculation]
-
-            meterscores_per_class[classifier_name][classname] = calculate_meterscore_for_class_based_on_tweets(classifier_name,classname,tweets_to_use_for_meter_score)
-
     return render(request,'result_overview.html',{'twitter_user':user,
                                                   'classifier_sections':ClassifierSection.objects.all().order_by('position'),
-                                                  'most_extreme_tweets':most_extreme_tweets,
-                                                  'meterscores_per_class': meterscores_per_class,
                                                   'profile_image_url':get_profile_image_url(user,settings.PASSWORD_FOLDER )})
